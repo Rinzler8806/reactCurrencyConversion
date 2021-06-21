7a16bd3a58f56935a2e44fe5496c0a8cf6e80704
Rinzler8806 <shane.rogers916@gmail.com>
2021-06-20 20:59:51 -0700

    api key from exchangeratesapi.io added

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-export default function CurrencyRow() {
+export default function CurrencyRow(props) {
+    const {
+        currencyOptions,
+        selectedCurrency
+    } = props
     return (
         <div>
-            <input type="number" className="input"/>
-            <select>
-                <option value="Hi">Hi</option>
+            <input type="number" className="input" />
+            <select value={selectedCurrency}>
+                {currencyOptions.map(option => (
+                    <option key={option} value={option}>{option}</option>
+                ))}
             </select>
         </div>
     )
